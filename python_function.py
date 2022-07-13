e5681a2caf18754e0eaf61f7e05d818ff1f529d1
Ryan Bannon <rbannon@imgs.ie>
2022-07-13 21:51:08 +0100

    updated python function

--- python_function.py
+++ python_function.py
@@ -1,5 +1,20 @@
-def main(args):
-    name = args.get("name", "stranger")
-    greeting = "Hello " + name + "!"
-    print(greeting)
-    return {"greeting": greeting}
+from datetime import datetime
+import random
+
+def main():
+    startNumber = 1
+    endNumber = 10000
+    prime = []
+    for i in range(startNumber,endNumber):
+        for p in range(2, i):
+            if (i % p)==0:
+                break
+        else:
+            prime.append(i)
+    num = random.sample(prime, 1)
+    now = datetime.now()
+    string = str("The random prime number this "+now.strftime("%A")+" at "+now.strftime("%R")+" is: "+str(num[0]))
+    print(string)
+    return string
+
+main()
