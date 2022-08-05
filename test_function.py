c67edf5479698ee1e79971565ea7ea73fb7349c9
Ryan Bannon <rbannon@imgs.ie>
2022-08-05 14:11:20 +0100

    preparing for final experiments

---
+++ test_function.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+import random
+
+startNumber = 1
+endNumber = 10000
+prime = []
+for i in range(startNumber,endNumber):
+    for p in range(2, i):
+        if (i % p)==0:
+            break
+    else:
+        prime.append(i)
+num = random.sample(prime, 1)
+now = datetime.now()
+message = str("The random prime number this "+now.strftime("%A")+" at "+now.strftime("%R")+" is: "+str(num[0]))
+print(message)
